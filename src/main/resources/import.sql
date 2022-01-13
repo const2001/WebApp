29e3502ca28048416872c71e5d3e90ab5a8ddd9f
const2001 <konos2001@hotmail.com>
2022-01-14 01:02:38 +0200

    Spring boot security and Hardcoded Data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+--Student Hardcoded data
 INSERT INTO users (id,active,password,roles,user_name) VALUES (1,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
 INSERT INTO users (id,active,password,roles,user_name) VALUES (2,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
 INSERT INTO users (id,active,password,roles,user_name) VALUES (3,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
