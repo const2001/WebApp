d88c21d202739c8ad42cc7fcc0c58c658828da00
const2001 <konos2001@hotmail.com>
2022-01-14 23:41:10 +0200

    Spring boot security and Hardcoded Data

--- src/main/java/hua/WebApp/SpringBoot/Application.java
+++ src/main/java/hua/WebApp/SpringBoot/Application.java
@@ -1,7 +1,10 @@
 package hua.WebApp.SpringBoot;
 
+import hua.WebApp.SpringBoot.entities.User.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -14,11 +17,30 @@ public class Application {
 
 	}
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 
 
+
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository userRepository) {
+		return args -> {
+
+
+			//userRepository.save(new User());
+
+
+
+
+
+
+		};
+	}
+
+
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,11 @@
 --Student Hardcoded data
-INSERT INTO users (id,active,password,roles,user_name) VALUES (1,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
-INSERT INTO users (id,active,password,roles,user_name) VALUES (2,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
-INSERT INTO users (id,active,password,roles,user_name) VALUES (3,true,'$2a$12$hg4Pga2WLDh4bUcwK09RvOaF10pql2klLBopuy/0CHtnorSwshble','ROLE_USER','john');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (1,true,'$2a$12$G58.q0KVfATzsD.foNJTD.KlFJYjVOOJQ8ubsfoIKqYuF2cYM/BSm','ROLE_STUDENT','john');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (2,true,'$2a$12$OJJ1z4bx5ArPZvEBi1RdE.htMf7.zEEk9YXl9Pf99.i8jbex7GtfO','ROLE_USER','kostas');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (3,true,'$2a$12$W8.ZJUcqTufMYl/AKCTh6ehA7dT5uQK4AqYVqv3lW1Vs4zCELfwDG','ROLE_USER','xristos');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (4,true,'$2a$12$J/4ALbLpd66a7R14lx/rwO2pThUJnxI6zAVEUksNcyiYdEoTGJoy.','ROLE_USER','nikos');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (5,true,'$2a$12$o2hwVEFqTdB9lDFcJEqgAucfSC1G6qNfRJ5NJfqSg1J3jZZul5iRq','ROLE_USER','dimitra');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (6,true,'$2a$12$gRil33laJJFTOrkpRJHSq.AKJVZhMkyIm5iIi0ehO/pBCoshlm6Q.','ROLE_USER','maria');
+--Teacher Hardcoded data
+INSERT INTO users (id,active,password,roles,user_name) VALUES (7,true,'$2a$12$dNbwI3odLUjKufj0rvBAWeksCXVbQTZxlmjbIUI2kq5ZrSzgcIZNq','ROLE_USER','kyrios.vaggelis');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (8,true,'$2a$12$08Cqo3u92S5bC4pFNyyR.e75YJWh68S/3R4XYMj279HNIOa4UpXHO','ROLE_USER','kyria.xristina');
+INSERT INTO users (id,active,password,roles,user_name) VALUES (9,true,'$2a$12$9M5duviXehpxEHs8hwBNdewz5/4/F49VEyDvXhmb1Ar7KqaYGUmte','ROLE_USER','kyrios.adonis');
