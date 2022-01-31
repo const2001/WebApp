ada4c41998425ee21911d28d875da864663d0376
Eleni Tsaousi <elenitsaousi@gmail.com>
2022-01-31 19:24:19 +0200

    professorPage

--- src/main/resources/static/email.js
+++ src/main/resources/static/email.js
@@ -1,16 +1,34 @@
 window.addEventListener("DOMContentLoaded", function() {
     //get the form elements defined in your form HTML above
+    //check if an element is empty
+    if (document.getElementById('name').value == "") {
+        alert("complete all the fields!")
+        document.getElementById('name').value
+        return false
+    }
+    if (document.getElementById('email').value == "") {
+        alert("complete all the fields!")
+        document.getElementById('email').value
+        return false
+    }
+    if (document.getElementById('letter').value == "") {
+        alert("complete all the fields")
+        document.getElementById('letter').value
+        return false
+    }
 
     var form = document.getElementById("my-form");
 
     var status = document.getElementById("status");
 
+    function error(){
+        status.innerHTML="Put correct email...";
+    }
+
+
     function success(){
         form.reset();
         status.innerHTML="Thanks!";
-    }
-    function error(){
-        status.innerHTML="Put correct email...";
     }
 
     //handle the form submission event
@@ -49,19 +67,20 @@ function checkforblank(){
     }
 }
 
-function ajax(method, url, data, success, error){
+function ajax(method, url, data, success, error) {
 
 
-    var xhr=new XMLHttpRequest();
-    xhr.open(method,url);
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange=function(){
-        if(xhr.readyState!==XMLHttpRequest.DONE) return;
-        if(xhr.status===200){
-            success(xhr.response,xhr.responseType);
-        }else{
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
             error(xhr.status, xhr.response, xhr.responseType);
         }
     };
     xhr.send(data);
+
 }
